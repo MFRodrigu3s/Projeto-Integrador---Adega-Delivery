1fd0a6f37a8ee696f8e18bed1e7455d01910dbe6
MFRodrigu3s <m.f3rreirar@gmail.com>
2022-05-21 21:58:07 -0300

    funçao cadastrar usuario funcionando (n validada)

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,4 @@
-const usuariosModel = require("../models/cadastro.json")
+const {Usuario} = require("../models/index")
 
 const usuarioController = {
     telaLogin: (req, res) => {
@@ -10,11 +10,16 @@ const usuarioController = {
     telaCadastro: (req, res) => {
         res.render("cadastro")
     },
-    realizarCadastro: (req, res) => {
-        let novoUsuario = req.body
-        console.log(novoUsuario)
-        usuariosModel.push(novoUsuario)
-        res.send(usuariosModel)
+    realizarCadastro: async (req, res) => {
+        let {nome_completo, email, cpf, senha} = req.body
+        const criarUsuario = await Usuario.create({
+            nome_completo: nome_completo,
+            cpf: cpf,
+            email: email,
+            senha: senha
+        })
+        console.log(criarUsuario)
+        res.send('Cadastrado!')
     },
     telaMinhaConta: (req, res) => {
         res.render('minhaConta')
