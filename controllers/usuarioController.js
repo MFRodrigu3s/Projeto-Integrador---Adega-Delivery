50be27c4ff0f72bfa4a9d0260ad42e199249b2f7
MFRodrigu3s <m.f3rreirar@gmail.com>
2022-05-27 21:34:25 -0300

    erros de validação corrigidos (codigo n quebra +)

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -8,36 +8,54 @@ const usuarioController = {
     },
     realizarLogin: async (req, res) => {
         const {email, senha} = req.body
-        const acharUsuario = await Usuario.findOne({where: {email: email}})
-        if(acharUsuario){
-            const compararSenha = bcrypt.compareSync(senha, acharUsuario.senha)
-            if(compararSenha){
-                res.send("logado!")
+        if(email, senha){
+            const acharUsuario = await Usuario.findOne({where: {email: email}})
+            if(acharUsuario){
+                const compararSenha = bcrypt.compareSync(senha, acharUsuario.senha)
+                if(compararSenha){
+                    // req session
+                    res.redirect("/usuario/minhaconta")
+                } else {
+                    res.send("Senha Incorreta!")
+                }
             } else {
-                res.send("Senha Incorreta!")
-            }
+                res.send("Usuario não encontrado! Por favor, verifique o email inserido")
+            }                                                                                                                                                                                                                                           
         } else {
-            res.send("Usuario não encontrado! Por favor, verifique o email inserido")
-        }                                                                                                                                                                                                                                           
+            res.send("Por favor, preencha os dados necessários")
+        }
     },
     telaCadastro: (req, res) => {
         res.render("cadastro")
     },
     realizarCadastro: async (req, res) => {
         let {nome, sobrenome, email, cpf, senha} = req.body
-        const hash = bcrypt.hashSync(senha, saltRound)
-        const criarUsuario = await Usuario.create({
-            nome: nome,
-            sobrenome: sobrenome,
-            cpf: cpf,
-            email: email,
-            senha: hash
-        })
-        console.log(criarUsuario)
-        res.send('Cadastrado!')
+        if(nome, sobrenome, email, cpf, senha){
+            const hash = bcrypt.hashSync(senha, saltRound)
+            const criarUsuario = await Usuario.create({
+                nome: nome,
+                sobrenome: sobrenome,
+                cpf: cpf,
+                email: email,
+                senha: hash
+            })
+            if(criarUsuario){
+                res.redirect('/usuario/minhaconta')
+            } else {
+                res.send("deu errado!")
+            }
+        } else {
+            res.send("Por favor, insira os dados corretamente.")
+        }
     },
     telaMinhaConta: (req, res) => {
+        // const pesquisa = Usuario.findOne({where: {email:email}})
         res.render('minhaConta')
+    },
+    updateMinhaConta: async (req, res) => {
+        // const atualizacao = Usuario.update({
+
+        // })
     }
 }
 
