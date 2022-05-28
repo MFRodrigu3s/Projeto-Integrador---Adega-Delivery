ba8460b2e9e7f272936769c55159132850babd6b
MFRodrigu3s <m.f3rreirar@gmail.com>
2022-05-27 21:54:57 -0300

    rota criada e view 'Catalogo' virou ejs

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,6 +1,9 @@
 const indexController = {
     paginaInicial: (req, res) => {
         res.render('paginaInicial')
+    },
+    catalogo: (req, res) => {
+        res.render('catalogo')
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 const indexController = require('../controllers/indexController')
 
 router.get('/', indexController.paginaInicial)
+router.get('/produtos', indexController.catalogo)
 
 module.exports = router;
