29475b75b51ecf69ff3508b26394a8c0f727ced7
MFRodrigu3s <m.f3rreirar@gmail.com>
2022-05-30 11:46:42 -0300

    renderizando array de produtos na view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,14 @@
+const {sequelize, Produtos} = require("../models/index")
+
 const indexController = {
     paginaInicial: (req, res) => {
         res.render('paginaInicial')
     },
-    catalogo: (req, res) => {
-        res.render('catalogo')
-    }
+    catalogo: async (req, res) => {
+        const pesquisa = await Produtos.findAll()
+        res.render("catalogo", {pesquisa})
+    },
+    
 }
 
 module.exports = indexController
