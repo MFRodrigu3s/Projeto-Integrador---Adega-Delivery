53ce10388682926400a0eb48d547225e67eb7177
MFRodrigu3s <m.f3rreirar@gmail.com>
2022-06-02 22:49:21 -0300

    getItem busca foto tambem e innerHTML gerando div

--- public/js/domCarrinho.js
+++ public/js/domCarrinho.js
@@ -1,13 +1,28 @@
 function getCarrinho() {
 
+    let div = document.getElementById("area-produtos")
+
     for(let i = 1; i<=99; i++){
 
         let nome = localStorage.getItem("produto" + i)
-        let quanditade = localStorage.getItem("qntdProd" + i)
+        let quantidade = localStorage.getItem("qntdProd" + i)
         let valorTdP = localStorage.getItem("valorTdP" + i)
+        let foto = localStorage.getItem("foto" + i)
 
-            console.log(nome, quanditade, valorTdP)
+            console.log(nome, quantidade, valorTdP)
+
+if(nome){
+
+    div.innerHTML += `<div class="produto-und">
+        <img class="foto" src=${foto} alt="${nome}"></img>
+        <p class="informacao">${nome}</p>
+        <p class="informacao">${quantidade}und</p>
+        <p class="informacao">R$ ${valorTdP}</p>
+</div>`
+}
 
     }
     
 }
+
+getCarrinho()
