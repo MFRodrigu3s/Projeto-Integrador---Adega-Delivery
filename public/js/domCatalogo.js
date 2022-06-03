2af93e8e4c29737061525c91ab7952e52bae8e29
MFRodrigu3s <m.f3rreirar@gmail.com>
2022-06-02 22:47:55 -0300

    foto do prod adicionada no local Storage

--- public/js/domCatalogo.js
+++ public/js/domCatalogo.js
@@ -1,7 +1,8 @@
-function AddCarrinho(id, nome, valor, quantidade){
+function AddCarrinho(foto, id, nome, valor, quantidade){
     localStorage.setItem("produto"+id, nome)
     localStorage.setItem("qntdProd"+id, quantidade)
     const valorTdP = valor * quantidade
     localStorage.setItem('valorTdP'+id, valorTdP)
+    localStorage.setItem('foto'+id, foto)
     alert('Produto adicionado ao carrinho!')
 }
