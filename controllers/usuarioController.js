bae3292a06ebea8d076f1221bc9a73d92b60785d
MFRodrigu3s <m.f3rreirar@gmail.com>
2022-06-13 14:45:38 -0300

    tentativa de descobrir erro no cadastro

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,4 @@
-const {Usuario} = require("../models/index")
+const { Usuario } = require("../models/index")
 const bcrypt = require("bcrypt")
 const saltRound = 8
 
@@ -7,12 +7,12 @@ const usuarioController = {
         res.render('login')
     },
     realizarLogin: async (req, res) => {
-        const {email, senha} = req.body
-        if(email, senha){
-            const acharUsuario = await Usuario.findOne({where: {email: email}})
-            if(acharUsuario){
+        const { email, senha } = req.body
+        if (email, senha) {
+            const acharUsuario = await Usuario.findOne({ where: { email: email } })
+            if (acharUsuario) {
                 const compararSenha = bcrypt.compareSync(senha, acharUsuario.senha)
-                if(compararSenha){
+                if (compararSenha) {
                     req.session.usuario = email
                     res.redirect("/usuario/minhaconta")
                 } else {
@@ -20,7 +20,7 @@ const usuarioController = {
                 }
             } else {
                 res.send("Usuario não encontrado! Por favor, verifique o email inserido")
-            }                                                                                                                                                                                                                                           
+            }
         } else {
             res.send("Por favor, preencha os dados necessários")
         }
@@ -29,51 +29,52 @@ const usuarioController = {
         res.render("cadastro")
     },
     realizarCadastro: async (req, res) => {
-        let dadosRecebidos = {nome, sobrenome, email, cpf, senha} = req.body
-        if(dadosRecebidos){
+        let dadosRecebidos = {nome, email, sobrenome, senha, cpf} = req.body
+        if (dadosRecebidos) {
+            console.log(dadosRecebidos)
             const hash = bcrypt.hashSync(senha, saltRound)
             const criarUsuario = await Usuario.create({
                 nome: nome,
-                sobrenome: sobrenome,
-                cpf: cpf,
                 email: email,
-                senha: hash
+                sobrenome: sobrenome,
+                senha: hash,
+                cpf: cpf
             })
-            if(criarUsuario){
+            if (criarUsuario) {
                 res.redirect('/usuario/minhaconta')
             } else {
                 res.send("deu errado!")
             }
         } else {
-            res.send("algo deu errado!")
+            res.send("preencha os dados corretamente")
         }
     },
     telaMinhaConta: async (req, res) => {
         let email = req.session.usuario
-        let usuario = await Usuario.findOne({where: {email:email}})
-        res.render('minhaConta', {usuario})
+        let usuario = await Usuario.findOne({ where: { email: email } })
+        res.render('minhaConta', { usuario })
     },
     updateMC: async (req, res) => {
-       let dados = {nome, sobrenome, email, cpf, senha} = req.body
-       if(dados){
-           const hash = bcrypt.hashSync(senha, saltRound)
-           const atualizarUsuario = await Usuario.update({
-               nome: nome,
-               sobrenome: sobrenome,
-               cpf: cpf,
-               email: email,
-               senha: hash
-           }, {
-               where: {email: email}
-           })
-           if(atualizarUsuario){
-               res.redirect('/usuario/minhaconta')
-           } else {
-               res.send("deu errado!")
-           }
-       } else {
-           res.send("insira os dados!")
-       }
+        let dados = { nome, sobrenome, email, cpf, senha } = req.body
+        if (dados) {
+            const hash = bcrypt.hashSync(senha, saltRound)
+            const atualizarUsuario = await Usuario.update({
+                nome: nome,
+                sobrenome: sobrenome,
+                cpf: cpf,
+                email: email,
+                senha: hash
+            }, {
+                where: { email: email }
+            })
+            if (atualizarUsuario) {
+                res.redirect('/usuario/minhaconta')
+            } else {
+                res.send("deu errado!")
+            }
+        } else {
+            res.send("insira os dados!")
+        }
     }
 }
 
