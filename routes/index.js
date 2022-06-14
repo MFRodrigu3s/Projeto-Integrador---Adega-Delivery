2344003e7d9c9b43ac6efbbb063265aa54ad61f3
MFRodrigu3s <m.f3rreirar@gmail.com>
2022-06-14 16:11:41 -0300

    middleware de login para poder comprar itens

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,14 @@ var router = express.Router();
 
 const indexController = require('../controllers/indexController')
 
+const logadoMiddleware = require("../middlewares/usuarioLogado")
+
 router.get('/', indexController.paginaInicial)
 router.get('/produtos', indexController.catalogo)
 router.get('/carrinho', indexController.carrinho)
-router.get('/pagamento', indexController.pagamento)
+router.get('/pagamento', logadoMiddleware, indexController.pagamento)
 router.get('/aprovandoPag', indexController.aprovandoPag)
+router.post('/aprovandoPag', indexController.aprovandoPag)
 router.get('/separandoPedido', indexController.separandoPedido)
 
 module.exports = router;
